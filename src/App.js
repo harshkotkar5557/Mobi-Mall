2791169122f38d2361446e8dba63367bcb9ae8c0
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2022-03-24 23:27:59 +0530

    add funtionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Product from './pages/product/product';
+import Cart from './pages/cart/cart'
 function App() {
   return (
     <div className="App">
       <Router>
       <Navbar/>
         <Routes>
-          <Route path='products' element={<Product/> }/>
+          <Route path='products' element={<Product />} />
+          <Route path='/cart' element={<Cart/> }/>
         </Routes>
       </Router>
     </div>
